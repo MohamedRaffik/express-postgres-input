736aec4bd1a773924864e637cb2e64f23d0f1660
Mohamed Raffik <raffikmohamed7@gmail.com>
2019-01-14 22:20:04 -0500

    -

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const api = require('./router');
+const api = require('./routes/api');
 
 app.use(express.json());
 app.use('/api', api);
